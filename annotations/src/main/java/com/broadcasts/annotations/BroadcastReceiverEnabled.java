c15c3af72e6cbe76b209415f51cef6bacd75a2dd
devggaurav <42926809+devggaurav@users.noreply.github.com>
2018-11-22 12:47:24 +0530

    Update BroadcastReceiverEnabled.java

--- annotations/src/main/java/com/broadcasts/annotations/BroadcastReceiverEnabled.java
+++ annotations/src/main/java/com/broadcasts/annotations/BroadcastReceiverEnabled.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
  * an annotation that determines the Broadcast Receiver enabled flag in manifest, which is
  * {@code true} by default
  * <p>
- * Created by Ahmed Adel Ismail on 1/20/2018.
+ * 
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)

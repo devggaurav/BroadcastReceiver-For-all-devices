a95b58b74a816a9e2aa27fb789bd84772ef19f4a
devggaurav <42926809+devggaurav@users.noreply.github.com>
2018-11-22 12:46:21 +0530

    Update BroadcastReceiverDataMimeType.java

--- annotations/src/main/java/com/broadcasts/annotations/BroadcastReceiverDataMimeType.java
+++ annotations/src/main/java/com/broadcasts/annotations/BroadcastReceiverDataMimeType.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
  * an annotation that determines the Broadcast Receiver data MIME type in manifest, and
  * it is also the same as calling {@code IntentFilter.addDataType(String)}
  * <p>
- * Created by Ahmed Adel Ismail on 1/20/2018.
+ * 
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)

ac8d6293493566b1230062a20139df1b50ee8a59
devggaurav <42926809+devggaurav@users.noreply.github.com>
2018-11-22 12:47:07 +0530

    Update BroadcastReceiverDataScheme.java

--- annotations/src/main/java/com/broadcasts/annotations/BroadcastReceiverDataScheme.java
+++ annotations/src/main/java/com/broadcasts/annotations/BroadcastReceiverDataScheme.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
  * an annotation that determines the Broadcast Receiver data SCHEME in manifest, and
  * it is also the same as calling {@code IntentFilter.addDataScheme(String)}
  * <p>
- * Created by Ahmed Adel Ismail on 1/20/2018.
+ * 
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
